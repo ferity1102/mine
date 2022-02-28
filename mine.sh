d930d616bd3a729f2c4eaefe60dc397f7ed1eeb6
ferity1102 <92764631+ferity1102@users.noreply.github.com>
2022-02-28 16:29:53 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod +x hellminer
 chmod +x verus-solver
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RAtp3eqwa4eQe1oQh51udKbqekiEhoLjVR.CPUMiner -p x --cpu 8
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RAtp3eqwa4eQe1oQh51udKbqekiEhoLjVR.CPUMiner -p x --cpu 4
